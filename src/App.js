32581f638b96c1faf864b9e0a97f553c3b87c988
MatanP <matan.pazualo@lightapp.com>
2020-04-29 13:38:26 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Modal from './Modal'
+class App extends React.Component {
+  state = { show: false };
+  componentDidMount(){
+    this.showModal()
+  }
+  showModal = () => {
+    this.setState({ show: true });
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  hideModal = () => {
+    this.setState({ show: false });
+  };
+
+  render() {
+    return (
+      <main className="App">
+        <h1>Modal As Alert</h1>
+        <Modal show={this.state.show} handleClose={this.hideModal}>
+          <p>im Alive</p>
+        </Modal>
+      </main>
+    );
+  }
 }
 
 export default App;
---
+++ src/Modal.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+  const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <button onClick={handleClose}>Ok</button>
+      </section>
+    </div>
+  );
+};
+export default Modal
